f31d5ea8cc4564f349ef45551d77a921f22d20e2
Your Name <you@example.com>
2019-11-23 16:39:03 +0200

    baas

---
+++ app.js
@@ -0,0 +1,12 @@
+// document meetodid
+// single element selectors
+
+sisu = document.getElementById('add-task'); // html sisu
+id = document.getElementById('add-task').id; // id 
+klass = document.getElementById('add-task').className; 
+
+sisu.style.background = '#333';
+sisu.style.color = '#fff';
+sisu.style.padding = '5px';
+
+console.log(sisu)
