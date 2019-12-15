fa50695dad157873b7c274056e6b1a972ed43b96
Your Name <you@example.com>
2019-12-15 13:54:05 +0200

    poolik/ lisab listi ja kustutab

--- app.js
+++ app.js
@@ -1,12 +1,87 @@
-// document meetodid
-// single element selectors
+document.getElementById('collection').innerHTML = localStorage.getItem('items');
 
-sisu = document.getElementById('add-task'); // html sisu
-id = document.getElementById('add-task').id; // id 
-klass = document.getElementById('add-task').className; 
+document.getElementById('add').addEventListener('click', lisaTabelisse)
 
-sisu.style.background = '#333';
-sisu.style.color = '#fff';
-sisu.style.padding = '5px';
+function lisaTabelisse(e){
+    const rida = document.createElement('li');
+    
+    const ylesanne = document.getElementById('task');
+    rida.innerHTML = ylesanne.value;
+    tabel = document.getElementById('collection');
+    
+    
+    if (ylesanne.value === ''){
+        alert('add task');
+    } else {
+        tabel.appendChild(rida);
+        let todosUl = document.querySelector('ul');
+        let a = document.createElement('a');
+        let i = document.createElement('i');
 
-console.log(sisu)
+        rida.className = "collection-item";
+        a.className = "secondary-content delete-item";
+        a.href = "#";
+        i.className = "fa fa-remove";
+        
+        todosUl.appendChild(rida);
+        rida.appendChild(a);
+        a.appendChild(i);
+        kustuta();
+        
+    }
+    document.getElementById('task').value = '';
+
+    
+    
+}
+
+function kustuta(){
+    let del  = document.querySelectorAll('.delete-item');
+    for (let element of del){
+        element.addEventListener('click', function(){element.parentNode.remove();
+        //localStorage.removeItem(element.parentNode.textContent);
+        
+        })
+      }
+      
+ }
+
+ document.querySelector('.clear-tasks').addEventListener('click', kustutaKoik);
+
+function kustutaKoik(){
+    document.getElementById('collection').innerHTML = '';
+    localStorage.clear();
+  }
+
+
+const lisa = document.getElementById('add');
+
+
+ lisa.addEventListener('click', function(e){
+    
+    const uusYlesanne = document.getElementById('task').value;
+    console.log(uusYlesanne)
+    let ylesanded; 
+    
+    if (localStorage.getItem('items') === null){
+        ylesanded = []
+    } else {
+        ylesanded = JSON.parse(localStorage.getItem('items'))
+        
+    }
+    
+    ylesanded.push(uusYlesanne);
+    localStorage.setItem('items', JSON.stringify(ylesanded));
+    
+    e.preventDefault();
+})
+
+
+
+
+
+
+
+
+
+
